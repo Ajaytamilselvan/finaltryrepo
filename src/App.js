75fdc57675f39aa2c7a7cd9d4722748e7c4d511c
ajay <ajaytamil12@gmail.com>
2025-01-12 09:24:30 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Nav from './component/nav/Nav';
+// import Login from './component/login/Login.jsx'
+import Log from './component/login/Log.jsx';
+import Todo from './component/todo/Todo.jsx';
+import Xogame from './component/xogame/Xogame.jsx';
+import Weather from './component/weather/Weather.jsx';
+
+
+
+
+function App() {
+  return (
+    <>
+      {/* <Nav />
+      <Login /> */}
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Log/>} />
+          <Route path='/Nav' element={<Nav/>} />
+          <Route path='/todo' element={<Todo/>} />
+          <Route path='/xogame' element={<Xogame/>} />
+          <Route path='/weather' element={<Weather/>} />
+          
+
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
